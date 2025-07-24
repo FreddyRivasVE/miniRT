be7003ebe29516c97e64b7c7fbbf6aac9d051115
briveraarg <brivera@student.42madrid.com>
2025-07-24 17:50:41 +0200

    open file

--- codigo/include/minirt.h
+++ codigo/include/minirt.h
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brivera <brivera@student.42.fr>            #+#  +:+       +#+        */
+/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-07-17 13:14:15 by brivera           #+#    #+#             */
-/*   Updated: 2025-07-17 13:14:15 by brivera          ###   ########.fr       */
+/*   Created: 2025/07/17 13:14:15 by brivera           #+#    #+#             */
+/*   Updated: 2025/07/24 17:24:37 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
-#define MINIRT_H
+# define MINIRT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 //libs42
 # include "../libs/libft/libft.h"
--- codigo/src/main.c
+++ codigo/src/main.c
@@ -3,17 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brivera <brivera@student.42madrid.com>     #+#  +:+       +#+        */
+/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-07-17 13:34:00 by brivera           #+#    #+#             */
-/*   Updated: 2025-07-17 13:34:00 by brivera          ###   ########.fr       */
+/*   Created: 2025/07/17 13:34:00 by brivera           #+#    #+#             */
+/*   Updated: 2025/07/24 17:46:37 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int main()
+void	ft_print_error(char *msj)
 {
-   ft_putstr_fd("arrancamos", 1);
-   return (0);
+	ft_putendl_fd(msj, 2);
+}
+
+int	mrt_check_file_type(char *file)
+{
+	size_t	len;
+
+	len = ft_strlen(file);
+	if (len < 3)
+		return (ft_print_error("Example: ./miniRT file.rt"), false);
+	if (ft_strncmp(&file[len - 3], ".rt", 3))
+		return (ft_print_error("Example: ./miniRT file.rt"), false);
+	return (true);
+}
+
+int	mrt_read_file(char *file)
+{
+	//char	*line;
+	int		fd;
+
+	if (!mrt_check_file_type(file))
+		return (false);
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (perror("Error"), false);
+	return (true);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (ft_print_error("Example: ./miniRT file.rt"), 1);
+	if (!mrt_read_file(argv[1]))
+		return (1);
+	return (0);
 }
